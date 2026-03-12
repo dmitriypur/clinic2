c3dbe78c910427fab8c4c34c2de262ff72ab481d
Дмитрий Пуртов <dmitriypur@MacBook-Pro-Dmitrij.local>
2026-03-12 13:07:09 +0300

    edit doctors card

--- resources/views/components/page/partials/doctor-card.blade.php
+++ resources/views/components/page/partials/doctor-card.blade.php
@@ -2,7 +2,14 @@
     <div class="flex gap-3 lg:gap-9">
         <div class="space-y-2 max-w-[138px] lg:max-w-60">
             <a href="{{ $doctor->url }}" class="rounded lg:rounded-none block overflow-clip">
-                {{ $doctor->avatar_image }}
+                @if($doctor->avatar_image)
+                    {{ $doctor->avatar_image }}
+                @else
+                <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-full opacity-30">
+                    <path d="M8 7C9.65685 7 11 5.65685 11 4C11 2.34315 9.65685 1 8 1C6.34315 1 5 2.34315 5 4C5 5.65685 6.34315 7 8 7Z" fill="#000000"/>
+                    <path d="M14 12C14 10.3431 12.6569 9 11 9H5C3.34315 9 2 10.3431 2 12V15H14V12Z" fill="#000000"/>
+                </svg>
+                @endif
             </a>
             <div class="flex lg:hidden gap-1 items-center justify-center bg-surface-subdued rounded p-1">
                 @for ($i = 0; $i < 5; $i++)
@@ -12,6 +19,7 @@
                 @endfor
             </div>
         </div>
+        
         <div class="flex flex-col justify-between">
             <div class="flex flex-col justify-between lg:justify-normal space-y-4 h-full lg:h-auto">
                 <div class="space-y-1 lg:space-y-3">
@@ -23,18 +31,22 @@
 
                     @if ($doctor->extra)
                         <div class="pt-1 lg:pt-0">
+                            @if(!empty($doctor->extra['seniority']))
                             <div class="flex gap-1 text-[13px] lg:text-base">
                                 <div class="text-interactive font-semibold">
                                     Стаж работы:
                                 </div>
                                 <div>{{ $doctor->extra['seniority'] }}</div>
                             </div>
+                            @endif
+                            @if(!empty($doctor->extra['category']))
                             <div class="flex gap-1 text-[13px] lg:text-base">
                                 <div class="text-interactive font-semibold">
                                     Категория:
                                 </div>
                                 <div>{{ $doctor->extra['category'] }}</div>
                             </div>
+                            @endif
                         </div>
                     @endif
                 </div>
--- resources/views/doctors/show.blade.php
+++ resources/views/doctors/show.blade.php
@@ -54,7 +54,15 @@
                         <div class="relative ">
                             <div
                                 class="[&_img]:relative [&_img]:-top-10 md:[&_img]:top-0 relative h-60 md:h-auto md:w-60 [&_img]:w-full overflow-clip rounded">
-                                {{ $doctor->avatar_image }}
+                                
+                                @if($doctor->avatar_image)
+                                    {{ $doctor->avatar_image }}
+                                @else
+                                <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-full opacity-30">
+                                    <path d="M8 7C9.65685 7 11 5.65685 11 4C11 2.34315 9.65685 1 8 1C6.34315 1 5 2.34315 5 4C5 5.65685 6.34315 7 8 7Z" fill="#000000"/>
+                                    <path d="M14 12C14 10.3431 12.6569 9 11 9H5C3.34315 9 2 10.3431 2 12V15H14V12Z" fill="#000000"/>
+                                </svg>
+                                @endif
                             </div>
                             <div
                                 class="absolute -bottom-[15px] h-[30px] inset-x-0 z-20 flex justify-center md:hidden gap-5 px-4">
