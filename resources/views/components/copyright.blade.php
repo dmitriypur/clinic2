94ba81cb8e2067fb79f6719ed5496ec9a459c5fa
Дмитрий Пуртов <dmitriypur@MacBook-Pro-Dmitrij.local>
2026-08-21 13:03:17 +0300

    edit copyringht

--- resources/views/components/copyright.blade.php
+++ resources/views/components/copyright.blade.php
@@ -1,4 +1,5 @@
+@php($details = $currentCity->details_items)
+
 <div class="text-xs md:text-lg font-medium md:text-center">
-    © {{ date('Y') }} ООО
-    «Аском».
+    © {{ date('Y') }} {{ $details[0]['name'] }}
 </div>
